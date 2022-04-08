6e1521f7a53ee50f27a096e17042bb3fb22b6ae3
JuliaFabris <yosoyjulia@gmail.com>
2022-04-08 00:11:34 -0300

    now the server serves

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
-require('dotenv').config()
+//require('dotenv').config()
 const express = require('express');
 const path = require('path');
 const app = express();
-const methodOverride =  require('method-override'); 
-const session = require('express-session');
-let cookieParser = require('cookie-parser');
-let cookieSession = require('./middlewares/cookieSession');
-let homeRouter = require('./routes/home');
-let adminrouter = require('./routes/admin');
+//const methodOverride =  require('method-override'); 
+//const session = require('express-session');
+//let cookieParser = require('cookie-parser');
+//let cookieSession = require('./middlewares/cookieSession');
+let homeRouter = require('./src/routes/home');
+//let adminrouter = require('./routes/admin');
 
-let userRouter = require('./routes/user');
-let faqRouter = require('./routes/faq');
+//let userRouter = require('./routes/user');
+//let faqRouter = require('./routes/faq');
 const { link } = require('fs');
 const PORT = 3000;
 
@@ -20,23 +20,18 @@ app.set('view engine', 'ejs');
 
 
 app.use(express.static(path.join(__dirname, '../public')));
-app.use(methodOverride('_method')); 
+//app.use(methodOverride('_method')); 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser())
-app.use(session({
-    secret: 'GrupoTresTriMovie',
-    resave: false,
-    saveUninitialized: true,
-}))
-app.use(cookieSession);
+//app.use(cookieParser())
+//app.use(session({
+    //secret: 'RíoCarcaraná',
+   // resave: false,
+   // saveUninitialized: true,
+//}))
+//app.use(cookieSession);
 
 app.use('/', homeRouter);
-app.use('/admin', adminrouter);
-app.use('/user', userRouter);
-app.use('/faq', faqRouter);
-
-app.use('/peliculas',require('./routes/peliculas'))
 
 
 app.use((req, res, next) => {
--- src/routes/home.js
+++ src/routes/home.js
@@ -4,9 +4,7 @@ let controller = require('../controllers/homeController')
 /* Get - pagina de inicio */
 router.get('/', controller.inicio);
 
-router.get('/peliculas/genero/:id', controller.filterByGenre);
-router.get('/peliculas/anio/:id', controller.filterByYear);
-router.get('/peliculas/search', controller.search);
+
 
 
 module.exports = router 
