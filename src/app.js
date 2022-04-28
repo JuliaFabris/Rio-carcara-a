3bb7f60f5dcfe4af5825a9cc4427e3fd9931fb17
DanielabGuardia <danielabguardia@gmail.com>
2022-04-27 23:45:00 -0300

    ordenado de app

--- src/app.js
+++ src/app.js
@@ -1,38 +1,24 @@
 const express = require('express');
-const path = require('path');
 const app = express();
-
+const path = require('path');
 const PORT = 3000;
 
+/* Enrutadores */
+let homeRouter = require('./routes/home');
+let backRouter = require('./routes/backRouter');
 
-app.set('views', path.resolve(__dirname,'views'));
+app.use(express.static('./public'));
+
 app.set('view engine', 'ejs');
+app.set('views', path.resolve(__dirname,'views'));
 
+/* Capturar informacion */
+app.use(express.urlencoded({ extended : false}));
+app.use(express.json())
 
-app.use(express.static(path.join(__dirname, './public')));
-//app.use(methodOverride('_method')); 
-
-app.use(express.urlencoded({ extended: false }));
-//app.use(cookieParser())
-//app.use(session({
-    //secret: 'RíoCarcaraná',
-   // resave: false,
-   // saveUninitialized: true,
-//}))
-//app.use(cookieSession);
-let homeRouter = require('./routes/home.js');
+/* rutas*/
 app.use('/', homeRouter);
-
-let backRouter = require('./routes/backRouter')
 app.use('/back', backRouter);
-
-/*app.use((req, res, next) => {
-    res.status(404).render('404')
-})*/
-
-/* const handleError = require('./middlewares/handleErrors')
-app.use(handleError); */
-// error handler
 
 
 app.listen(PORT, ()=>console.log(`Servidor levantado en el puerto ${PORT}
