19bb070bc2210cc0f29d04d3c5731c9e84d0b753
DanielaCalisaya <hdaniela127c@gmail.com>
2022-06-07 02:00:18 -0500

    added language-ejs

---
+++ public/js/googleTranslate.js
@@ -0,0 +1,7 @@
+/* function googleTranslateElementInit(){
+    new google.translate.TranslateElement({pageLanguage:'es',layout:google.translate.TranslateElement.InlineLayout.SIMPLE},'google_translate_element')
+} */
+
+function loadGoogleTranslate() {
+    new google.translate.TranslateElement("google_element");
+}
