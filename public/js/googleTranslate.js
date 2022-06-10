f7bcac1c5e7e4a6818750d5767aeac491856bc33
DanielaCalisaya <hdaniela127c@gmail.com>
2022-06-09 21:53:53 -0500

    googleTranslate

--- public/js/googleTranslate.js
+++ public/js/googleTranslate.js
@@ -1,21 +1,35 @@
 /* API Cloud Translation */
 
-let bannerTranslate = document.querySelector('.goog-te-banner');
-let bodysPages = document.querySelector('body')
+let bannerTranslate = document.querySelector('.goog-te-banner-frame');
+/* let bodysPages = document.querySelectorAll('.pages'); */
+/* let bodysPagesBottom = document.querySelectorAll('.pagesBottom');  */
 
 
 function loadGoogleTranslate() {
+
     new google.translate.TranslateElement({pageLanguage:'es', layout:google.translate.TranslateElement.SIMPLE}, "google_element");
-    if(bannerTranslate.style.display === 'block') {
-        
-    }
+    
+    /* if(bannerTranslate.style.display === 'block') {
+        bodysPages.classList.add('.pages')
+    }  */ /* {
+        bodysPagesBottom.classList.add('.pagesBottom')
+    }  */
 }
+/* else(bodysPages.classList.add('.pagesBottom')) */
+
+
+
+
+
+
+
+
+
 
 
 /* function googleTranslateElementInit(){
     new google.translate.TranslateElement({pageLanguage:'es',layout:google.translate.TranslateElement.InlineLayout.SIMPLE},'google_translate_element')
 } 
-
  if(typeof(document.querySelector) == 'goog-te-banner') {
         document.querySelector('.goog-te-banner').setAttribute('style', 'position: relative');
     }
